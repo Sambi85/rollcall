f3d85d22f943994764826010bec936ba80df1a2f
Sam Lesser <mac_2025@Sams-MBP.home>
2025-03-24 22:02:21 -0500

    touch up linting

--- app/models/creature.rb
+++ app/models/creature.rb
@@ -1,7 +1,7 @@
 class Creature < ApplicationRecord
   belongs_to :tracker, optional: true
 
-  validates :name, presence: true 
+  validates :name, presence: true
 
   after_initialize :set_defaults
 
