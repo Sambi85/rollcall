03ffb4dc3501dccff9d8dbf32c6ae192a261b0ab
Sam Lesser <mac_2025@Sams-MBP.home>
2025-03-28 21:01:46 -0500

    update tracker controller

--- app/controllers/tracker_controller.rb
+++ app/controllers/tracker_controller.rb
@@ -1,10 +1,25 @@
+# app/controllers/tracker_controller.rb
 class TrackerController < ApplicationController
+  before_action :set_tracker, only: [:next_round, :get_initiative_order, :get_dead_combatants]
+
   def next_round
+    @tracker.next_round
+    render json: { round: @tracker.round }, status: :ok
   end
 
   def get_initiative_order
+    order = @tracker.creatures.order(initiative_roll: :desc).pluck(:name)
+    render json: { initiative_order: order }
   end
 
   def get_dead_combatants
+    dead_combatants = @tracker.creatures.where(dead: true).pluck(:name)
+    render json: { dead_combatants: dead_combatants }
+  end
+
+  private
+
+  def set_tracker
+    @tracker = Tracker.find(params[:id])
   end
 end
