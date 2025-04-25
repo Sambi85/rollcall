2d4fd99dcd131d396d015591ffca0edfe317ba08
Sam Lesser <mac_2025@Sams-MBP.home.local>
2025-04-24 21:37:10 -0500

    add hp tracking tests to controller test suite

--- test/controllers/creatures_controller_test.rb
+++ test/controllers/creatures_controller_test.rb
@@ -32,4 +32,36 @@ class CreaturesControllerTest < ActionDispatch::IntegrationTest
     put tracker_creature_mark_alive_path(@tracker, @creature)
     assert_response :success
   end
+
+  test "should apply damage using receive_damage" do
+    put tracker_creature_receive_damage_path(@tracker, @creature), params: { amount: 8 }, as: :json
+    assert_response :success
+    @creature.reload
+    assert_equal 0, @creature.temp_hp
+    assert_equal 2, @creature.current_hp
+  end
+
+  test "should not reduce hp below 0 with receive_damage" do
+    put tracker_creature_receive_damage_path(@tracker, @creature), params: { amount: 50 }, as: :json
+    assert_response :success
+    @creature.reload
+    assert_equal 0, @creature.current_hp
+    assert @creature.dead
+  end
+
+  test "should heal creature and not exceed max_hp" do
+    @creature.update!(current_hp: 1)
+    put tracker_creature_heal_path(@tracker, @creature), params: { amount: 15 }, as: :json
+    assert_response :success
+    @creature.reload
+    assert_equal 10, @creature.current_hp
+  end
+
+  test "should not heal dead creature" do
+    @creature.update!(dead: true, current_hp: 0)
+    put tracker_creature_heal_path(@tracker, @creature), params: { amount: 5 }, as: :json
+    assert_response :success
+    @creature.reload
+    assert_equal 0, @creature.current_hp
+  end
 end
