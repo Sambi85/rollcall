e8594c9b60999779fc461152fd2a234e3f5ce04f
Sam Lesser <mac_2025@Sams-MBP.home.local>
2025-05-13 15:56:31 -0500

    add seeds for abilities

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@ Tracker.destroy_all
 Creature.destroy_all
 Effect.destroy_all
 SpecialEvent.destroy_all
+Ability.destroy_all
 
 # Trackers (Global)
 tracker = Tracker.create!(round: 1, turn_order: [])
@@ -34,6 +35,33 @@ SpecialEvent.create!(name: "Blizzard", description: "Bone chilling winds and hea
 
 # Add special event to tracker
 SpecialEvent.create!(name: "Toxic Mist", description: "Poison Mist, Con save DC 11", frequency: 15, tracker: tracker)
+
+# Add Abilities
+Ability.create!(
+  [
+    {
+      name: "Fireball",
+      description: "Deals fire damage to all enemies in a small area.",
+      usage_type: "limited",
+      default_usage_limit: 3,
+      default_cooldown: 0
+    },
+    {
+      name: "Heal",
+      description: "Restores health to an ally.",
+      usage_type: "cooldown",
+      default_usage_limit: 0,
+      default_cooldown: 2
+    },
+    {
+      name: "Berserk",
+      description: "Increase attack power but lose some defense.",
+      usage_type: "unlimited",
+      default_usage_limit: 0,
+      default_cooldown: 0
+    }
+  ]
+)
 
 # Creatures (Global)
 creatures = [
