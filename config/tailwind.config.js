a8667e01e00edf3d599461159df1879b0365292f
Sam Lesser <mac_2025@Sams-MBP.home.local>
2025-08-11 18:10:50 -0500

    restore dark.css

---
+++ config/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          bg: "#1e1e2f",
+          text: "#f1f1f1",
+          "text-muted": "#a1a1a1",
+          accent: "#10b981",
+          "card-bg": "#2a2a3b",
+          border: "#333",
+        },
+      },
+    },
+  }
+  
